fd7d779c6796c72fa92191a760bee0829010c432
KyssiKibb <kobiconn@gmail.com>
2024-11-25 17:50:02 -0800

    partials mean we don't need that weird casting stuff so i moved MCQuestion choices out of testquestionmodel again.
    Midterm done for real this time....

--- Midterm/Models/MultipleChoiceQuestionModel.cs
+++ Midterm/Models/MultipleChoiceQuestionModel.cs
@@ -6,7 +6,7 @@ namespace Midterm
 	{
 		[Required]
 		public virtual string Answer { get; set; }
-		//[Required] //when i have the choices here instead of in the base model it requires casting
-		//public List<string>? Choices { get; set; }
+		[Required] //when i have the choices here instead of in the base model it requires casting
+		public List<string>? Choices { get; set; }
 	}
 }
--- Midterm/Models/TestQuestionModel.cs
+++ Midterm/Models/TestQuestionModel.cs
@@ -11,7 +11,7 @@ namespace Midterm
     {
         public int ID { get; set; }
         public string? Question { get; set; }
-        public List<string>? Choices { get; set; }
+        //public List<string>? Choices { get; set; }
         [Required]
         public virtual string Answer { get; set; }
     }
