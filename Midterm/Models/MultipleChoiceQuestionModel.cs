bf505fc320bad422e1744278cdf5120585e28d79
KyssiKibb <kobiconn@gmail.com>
2024-11-25 21:32:42 -0800

    comment change :p

--- Midterm/Models/MultipleChoiceQuestionModel.cs
+++ Midterm/Models/MultipleChoiceQuestionModel.cs
@@ -6,7 +6,7 @@ namespace Midterm
 	{
 		[Required]
 		public virtual string Answer { get; set; }
-		[Required] //when i have the choices here instead of in the base model it requires casting
+		[Required] //when i have the choices here instead of in the base model it requires casting in TakeTest (unless you do partial view which does the casting)
 		public List<string>? Choices { get; set; }
 	}
 }
